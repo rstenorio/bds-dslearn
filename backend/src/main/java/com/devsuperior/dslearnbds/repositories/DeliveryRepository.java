26d1d5deacf25ce961f50eef84aa950071c6b063
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2021-06-10 09:53:06 +0200

    Repositories created

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Delivery;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long>{
+
+}
